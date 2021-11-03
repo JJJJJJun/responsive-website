0eb8f0021cc337f6fa8b6658da9c4f1782843f30
JJJJJJun <gmlwns21119@naver.com>
2021-11-03 17:47:13 +0900

    넷바 메뉴 클릭 이동 Js까지

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 //넷바 색상 변경
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -10,3 +11,35 @@ document.addEventListener("scroll", () => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+//넷바메뉴 선택시 해당 부분으로 이동하기
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link);
+});
+
+/*const homeContact = document.querySelector(".home__contact");
+homeContact.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link);
+});*/
+
+//contact me 버튼 이동하기
+const contactMeBtn = document.querySelector(".home__contact");
+contactMeBtn.addEventListener("click", () => {
+    scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : "smooth"})
+}
