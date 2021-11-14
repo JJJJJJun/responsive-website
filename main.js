21ea6d4c35dff0a68eb158f24a1b1611e464826c
JJJJJJun <gmlwns21119@naver.com>
2021-11-14 11:27:31 +0900

    arrowupJS

--- main.js
+++ main.js
@@ -30,11 +30,28 @@ contactMeBtn.addEventListener("click", () => {
     scrollIntoView("#contact");
 });
 
+// arrow-up 버튼 이동하기
+const arrowUp= document.querySelector(".arrow-up");
+arrowUp.addEventListener("click", ()=> {
+    scrollIntoView("#home");
+});
+
 //home 투명도 조절
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", ()=>{
     home.style.opacity = 1 - window.scrollY/homeHeight;
+});
+
+// arrow-up 투명도 조절
+const arrow = document.querySelector(".arrow-up"); // arrow-up 셀렉트
+document.addEventListener("scroll",()=>{ // 이벤트 발생
+    if(window.scrollY < 1){
+        arrow.style.opacity=0;
+    }
+    if(window.scrollY > homeHeight/2){ // 홈 높이에서 절반 사라질시
+        arrow.style.opacity=1;
+    }
 });
 
 //햄버거 메뉴 클릭 시 
